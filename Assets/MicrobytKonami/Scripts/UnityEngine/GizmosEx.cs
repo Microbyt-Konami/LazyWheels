2048d7870d23dd7512c6bd89a3e1a2d0e56173ba
bugbit <ohernandezba@gmail.com>
2024-01-31 23:47:28 +0100

    ray Up

---
+++ Assets/MicrobytKonami/Scripts/UnityEngine/GizmosEx.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine
+{
+    public static class GizmosEx
+    {
+        // https://discussions.unity.com/t/drawing-a-2d-capsule-same-of-capsulecollider2d/227921/3
+        public static void DrawCapsule2D(Transform transform, Vector3 offset, Vector2 size, CapsuleDirection2D direction)
+        {
+            float step = 0.2f;
+            float r = Mathf.Min(size.x, size.y) / 2;
+            float d = Mathf.Max(size.x, size.y) / 2 - r;
+            Vector3 lp = Vector3.positiveInfinity;
+            for (float a = -Mathf.PI; a <= Mathf.PI + step; a += step)
+            {
+                float x = Mathf.Cos(a);
+                float y = Mathf.Sin(a);
+                Vector3 np = offset + new Vector3(x, y) * r;
+                np += (direction == CapsuleDirection2D.Vertical ? Vector3.up : Vector3.zero) * d * Mathf.Sign(y);
+                np += (direction == CapsuleDirection2D.Horizontal ? Vector3.right : Vector3.zero) * d * Mathf.Sign(x);
+                np = transform.TransformPoint(np);
+                if (lp != Vector3.positiveInfinity) Gizmos.DrawLine(lp, np);
+                lp = np;
+            }
+        }
+
+        // https://forum.unity.com/threads/drawing-capsule-gizmo.354634/
+        public static void DrawWireCapsule(Vector3 p1, Vector3 p2, float radius)
+        {
+#if UNITY_EDITOR
+            // Special case when both points are in the same position
+            if (p1 == p2)
+            {
+                // DrawWireSphere works only in gizmo methods
+                Gizmos.DrawWireSphere(p1, radius);
+                return;
+            }
+            using (new UnityEditor.Handles.DrawingScope(Gizmos.color, Gizmos.matrix))
+            {
+                Quaternion p1Rotation = Quaternion.LookRotation(p1 - p2);
+                Quaternion p2Rotation = Quaternion.LookRotation(p2 - p1);
+                // Check if capsule direction is collinear to Vector.up
+                float c = Vector3.Dot((p1 - p2).normalized, Vector3.up);
+                if (c == 1f || c == -1f)
+                {
+                    // Fix rotation
+                    p2Rotation = Quaternion.Euler(p2Rotation.eulerAngles.x, p2Rotation.eulerAngles.y + 180f, p2Rotation.eulerAngles.z);
+                }
+                // First side
+                UnityEditor.Handles.DrawWireArc(p1, p1Rotation * Vector3.left, p1Rotation * Vector3.down, 180f, radius);
+                UnityEditor.Handles.DrawWireArc(p1, p1Rotation * Vector3.up, p1Rotation * Vector3.left, 180f, radius);
+                UnityEditor.Handles.DrawWireDisc(p1, (p2 - p1).normalized, radius);
+                // Second side
+                UnityEditor.Handles.DrawWireArc(p2, p2Rotation * Vector3.left, p2Rotation * Vector3.down, 180f, radius);
+                UnityEditor.Handles.DrawWireArc(p2, p2Rotation * Vector3.up, p2Rotation * Vector3.left, 180f, radius);
+                UnityEditor.Handles.DrawWireDisc(p2, (p1 - p2).normalized, radius);
+                // Lines
+                UnityEditor.Handles.DrawLine(p1 + p1Rotation * Vector3.down * radius, p2 + p2Rotation * Vector3.down * radius);
+                UnityEditor.Handles.DrawLine(p1 + p1Rotation * Vector3.left * radius, p2 + p2Rotation * Vector3.right * radius);
+                UnityEditor.Handles.DrawLine(p1 + p1Rotation * Vector3.up * radius, p2 + p2Rotation * Vector3.up * radius);
+                UnityEditor.Handles.DrawLine(p1 + p1Rotation * Vector3.right * radius, p2 + p2Rotation * Vector3.left * radius);
+            }
+#endif
+        }
+    }
+}
--- Assets/Scripts/Rays.cs
+++ Assets/Scripts/Rays.cs
@@ -9,7 +9,7 @@ namespace MicrobytKonami.LazyWheels
     {
         //Fields
         [SerializeField] private Transform rayUpPoint;
-        [SerializeField] private Transform rayUpDownPoint;
+        [SerializeField] private Transform rayDownPoint;
         [SerializeField] private Transform rayRightPoint;
         [SerializeField] private Transform rayLeftPoint;
         [SerializeField] private float timeRay = 3;
@@ -37,56 +37,12 @@ namespace MicrobytKonami.LazyWheels
             float w = ray.x / 2;
             float h = ray.y / 2;
 
+            //print($"ray : {ray.x} {ray.y}");
+            //print($"DrawCapsule2D({rayUpPoint}, {new Vector3(0, -h / 2f)}, {ray}");
             Gizmos.color = Color.yellow;
-            // https://discussions.unity.com/t/drawing-a-2d-capsule-same-of-capsulecollider2d/227921/3
-            // https://forum.unity.com/threads/drawing-capsule-gizmo.354634/
-            //Gizmos.DrawWireSphere(rayUpDownPoint.position + h * Vector3.up, h);
-            //Gizmos.DrawWireMesh
-            // collider is CapsuleCollider
-            //Gizmos.matrix = collider.transform.localToWorldMatrix;
-
-            //Vector3 offset = Vector3.zero;
-            //offset[collider.direction] = collider.height * 0.5f - collider.radius;
-            //DrawWireCapsule(collider.center + offset, collider.center - offset, collider.radius);
-
-        }
-
-        public static void DrawWireCapsule(Vector3 p1, Vector3 p2, float radius)
-        {
-#if UNITY_EDITOR
-            // Special case when both points are in the same position
-            if (p1 == p2)
-            {
-                // DrawWireSphere works only in gizmo methods
-                Gizmos.DrawWireSphere(p1, radius);
-                return;
-            }
-            using (new UnityEditor.Handles.DrawingScope(Gizmos.color, Gizmos.matrix))
-            {
-                Quaternion p1Rotation = Quaternion.LookRotation(p1 - p2);
-                Quaternion p2Rotation = Quaternion.LookRotation(p2 - p1);
-                // Check if capsule direction is collinear to Vector.up
-                float c = Vector3.Dot((p1 - p2).normalized, Vector3.up);
-                if (c == 1f || c == -1f)
-                {
-                    // Fix rotation
-                    p2Rotation = Quaternion.Euler(p2Rotation.eulerAngles.x, p2Rotation.eulerAngles.y + 180f, p2Rotation.eulerAngles.z);
-                }
-                // First side
-                UnityEditor.Handles.DrawWireArc(p1, p1Rotation * Vector3.left, p1Rotation * Vector3.down, 180f, radius);
-                UnityEditor.Handles.DrawWireArc(p1, p1Rotation * Vector3.up, p1Rotation * Vector3.left, 180f, radius);
-                UnityEditor.Handles.DrawWireDisc(p1, (p2 - p1).normalized, radius);
-                // Second side
-                UnityEditor.Handles.DrawWireArc(p2, p2Rotation * Vector3.left, p2Rotation * Vector3.down, 180f, radius);
-                UnityEditor.Handles.DrawWireArc(p2, p2Rotation * Vector3.up, p2Rotation * Vector3.left, 180f, radius);
-                UnityEditor.Handles.DrawWireDisc(p2, (p1 - p2).normalized, radius);
-                // Lines
-                UnityEditor.Handles.DrawLine(p1 + p1Rotation * Vector3.down * radius, p2 + p2Rotation * Vector3.down * radius);
-                UnityEditor.Handles.DrawLine(p1 + p1Rotation * Vector3.left * radius, p2 + p2Rotation * Vector3.right * radius);
-                UnityEditor.Handles.DrawLine(p1 + p1Rotation * Vector3.up * radius, p2 + p2Rotation * Vector3.up * radius);
-                UnityEditor.Handles.DrawLine(p1 + p1Rotation * Vector3.right * radius, p2 + p2Rotation * Vector3.left * radius);
-            }
-#endif
+            GizmosEx.DrawCapsule2D(rayUpPoint, new Vector3(0, -h), ray, CapsuleDirection2D.Vertical);
+            Gizmos.color = Color.red;
+            GizmosEx.DrawCapsule2D(rayDownPoint, new Vector3(0, h), ray, CapsuleDirection2D.Vertical);
         }
 
         // Start is called before the first frame update
